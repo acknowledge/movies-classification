0d150f3b28852d962b6e2ee5be4eefdc657c459f
acknowledge <jack_ett8@hotmail.com>
2015-03-14 19:07:04 +0100

    curvature_hist is done

--- code/curvature_hist.py
+++ code/curvature_hist.py
@@ -1,7 +1,26 @@
 def curvature_hist(img, step=10, plot=False):
+    n = 10
     gs = gridspec.GridSpec(2,2)
     cvt = curvature(img, step, plot, gs)
-    pl.subplot(gs[1, :])
-    pl.hist(cvt)
-    pl.title("histogram of curvatures")
-    return cvt
+    
+    if plot:
+        pl.subplot(gs[1, :])
+        pl.hist(cvt)
+        pl.title("histogram of curvatures")
+        
+    min = cvt.min()
+    max = cvt.max()
+    qty = cvt.size
+    bin_size = (max-min)/n
+    
+    bins = [0] * n
+
+    for i in range(0,n):
+        for e in cvt:
+            if e >= min+(i*bin_size):
+                if e < min+((i+1)*bin_size):
+                    bins[i] = bins[i]+1
+    for i in range(0,n):
+        bins[i] = bins[i]*1.0/qty
+
+    return bins
