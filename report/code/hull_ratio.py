bff2be795e9ea2beb8b7cc91f704fba807bf221a
acknowledge <jack_ett8@hotmail.com>
2015-04-21 12:37:36 +0200

    clean report for the new one

--- report/code/hull_ratio.py
+++
@@ -1,6 +0,0 @@
-def hull_ratio(img):
-    cnt = extract_contour(img)
-    hull = cv2.convexHull(cnt)
-    areaValid = cv2.contourArea(cnt)
-    hullArea = cv2.contourArea(hull)
-    return areaValid/hullArea
