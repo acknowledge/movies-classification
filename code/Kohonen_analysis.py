798961fa9e62e5f6efd4e96b3a2b6ddf7b89aa6c
Simoyw <simone.cogno@master.hes-so.ch>
2015-05-26 12:31:48 +0200

    rename exercice.py to Kohonen_analysis

--- code/Kohonen_analysis.py
+++ code/Kohonen_analysis.py
@@ -10,15 +10,15 @@ import matplotlib.pyplot as pl
 from matplotlib import cm
 from loadData import  readFile, extractArrays
 
-fname = 'data3/datacomedyadventure50.json'
+fname = 'data3/data10.json'
 
 infos = readFile(fname)
 titles, words, matrix = extractArrays(infos)
 matrix = np.transpose(matrix)
 
-pl.figure(figsize=(5,5))
-pl.xticks(np.arange(matrix.shape[1]), words, rotation=45)
-pl.yticks(np.arange(matrix.shape[0]), titles)
+pl.figure(figsize=(20,40))
+pl.xticks(np.arange(matrix.shape[1]*2), words, rotation=45)
+pl.yticks(np.arange(matrix.shape[0]*2), titles)
 pl.title("titles")
 pl.imshow(matrix, interpolation='nearest', cmap=cm.binary)
 _ = pl.savefig('images/titles.png')
@@ -28,7 +28,7 @@ import kohonen
 from Utils import computeUMatrix, constructSamplesForNeurons, ExponentialTimeseries as ET
 from random import shuffle
 
-## Initializes the Kohonen map as a rectangular map of len(titles) x len(titles)*2
+## define cosine metric for configure distance metrix on kohonen
 def cosine_metric(x, y):
     '''Returns the cosine distance between x and y.'''
     nx = np.sqrt(np.sum(x * x, axis=-1))
@@ -37,7 +37,7 @@ def cosine_metric(x, y):
     # orthogonal, and -1 when they are opposite. we want the opposite effect,
     # and we want to make sure the results are always nonnegative.
     return 1 - np.sum(x * y, axis=-1) / nx / ny
-
+## Initializes the Kohonen map as a rectangular map of len(titles) x len(titles)*2
 side = len(titles)
 side=side/2
 params = kohonen.Parameters(dimension=len(words), shape=(side,side*2), metric=cosine_metric)
@@ -62,7 +62,7 @@ colors = cm.Spectral_r
 colors.set_bad('w',1.)
 
 ## Gets winning neuron for each sample (1-nn)
-animalDict = constructSamplesForNeurons(kmap, matrix)
+filmDict = constructSamplesForNeurons(kmap, matrix)
 
 ## Prints U-Matrix
 fig = pl.figure(figsize=(20,30))
@@ -71,15 +71,15 @@ pl.imshow(u_matrix, cmap=colors, interpolation="nearest")
 pl.colorbar(shrink=0.2)
 
 ## Prints titles on map
-for neuron in animalDict:
+for neuron in filmDict:
     neuronx, neurony = neuron.split(",")
     neuronx = int(neuronx) * 2
     neurony = int(neurony) * 2
     pl.scatter(neurony, neuronx)
-    animalNames = ""
-    for animalID in animalDict[neuron]:
-        animalNames += titles[animalID] + "\n"
-    pl.annotate(animalNames, (neurony, neuronx), size=6)
+    filmNames = ""
+    for filmID in filmDict[neuron]:
+        filmNames += titles[filmID] + "\n"
+    pl.annotate(filmNames, (neurony, neuronx), size=6)
 
 pl.axis('off')
 
