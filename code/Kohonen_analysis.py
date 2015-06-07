95224f3925cb9beb1e670473b957033efa5e5783
Simoyw <simone.cogno@master.hes-so.ch>
2015-06-07 22:12:00 +0200

    Add some text

--- code/Kohonen_analysis.py
+++ code/Kohonen_analysis.py
@@ -12,7 +12,7 @@ from loadData import  readFile, extractArrays
 from sklearn.metrics.pairwise import pairwise_distances
 from MovieCategories import MovieCategories
 import pylab
-fname = 'data5/data100.json'
+fname = 'data5/data50.json'
 
 
 infos = readFile(fname)
@@ -73,7 +73,7 @@ for idx in range(10):
 print Z.shape
 image=dendrogram(Z,labels=titlesCat, distance_sort='descendent',
                  leaf_font_size=2, orientation='left', show_contracted=False)
-pylab.savefig("images/clustering100_tf_idf.png",dpi=300,bbox_inches='tight')
+pylab.savefig("images/clustering50_tf_idf_4.png",dpi=300,bbox_inches='tight')
 #cluster=fcluster(Z,0.6, depth=3)
 #_ = pl.savefig('images/clusters.png')
 
@@ -124,4 +124,4 @@ for neuron in filmDict:
 
 pl.axis('off')
 
-_ = pl.savefig("images/map100_tfidf_1.png", bbox_inches = 'tight')
+_ = pl.savefig("images/map50_tfidf_4.png", bbox_inches = 'tight')
